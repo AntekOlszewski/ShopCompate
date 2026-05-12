96ed191c64358f786238568d39233ef67d5aa6f0
antek <antek.olszewski01@gmail.com>
2026-05-12 17:01:37 +0200

    remove data volume

--- ShopCompare.AppHost/AppHost.cs
+++ ShopCompare.AppHost/AppHost.cs
@@ -1,7 +1,6 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var postgres = builder.AddPostgres("postgres")
-    .WithDataVolume("shopcompare")
     .AddDatabase("shopcompare-db");
 
 builder.AddProject<Projects.ShopCompare_Api>("api")
